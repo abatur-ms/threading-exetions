9ee1bac945188f4404c454dc0c85728976140503
Abir Atur <abatur@microsoft.com>
2020-07-20 16:24:29 +0300

    add thread pool and task scheduler

---
+++ src/Threading.Extensions/ThreadPool.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Threading.Extensions
+{
+    public interface IThreadPool<TState>
+    {
+        void Enqueue(Action<TState> action, TState state);
+    }
+
+    public class ThreadPool<TState> : IDisposable
+    {
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private readonly BlockingCollection<(Action<TState>, TState)> _queue = new BlockingCollection<(Action<TState>, TState)>(new ConcurrentQueue<(Action<TState>, TState)>());
+
+        public ThreadPool(int numberOfThreads, string poolName)
+        {
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                var thread = new Thread(Work)
+                {
+                    Name = $"{poolName}-{i}"
+                };
+
+                thread.Start();
+            }
+        }
+
+        public void Enqueue(Action<TState> action, TState state)
+        {
+            _queue.Add((action, state));
+        }
+
+        private void Work()
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                try
+                {
+                    (Action<TState> action, TState state) = _queue.Take(_cts.Token);
+
+                    try
+                    {
+                        action(state);
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                }
+                catch
+                {
+                    if (_cts.IsCancellationRequested)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _cts.Cancel();
+            _queue.CompleteAdding();
+        }
+    }
+}
---
+++ src/Threading.Extensions/ThreadPoolTaskScheduler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Threading.Extensions
+{
+    public class ThreadPoolTaskScheduler : TaskScheduler
+    {
+        private readonly IThreadPool<Task> _threadPool;
+        
+        public ThreadPoolTaskScheduler(IThreadPool<Task> threadPool)
+        {
+            _threadPool = threadPool;
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            _threadPool.Enqueue(ExecuteTask, task);
+        }
+
+        private void ExecuteTask(Task task)
+        {
+            TryExecuteTask(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            return false;
+        }
+    }
+}
